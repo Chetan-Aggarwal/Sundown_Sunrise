0eeafe43d128db2cb820b08faf90c7ceac1b2d05
Chetan Aggarwal <97167818+Chetan-Aggarwal@users.noreply.github.com>
2024-01-04 21:07:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+    });
+}
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+swiperAnimation()
+page4Animation()
+menuAnimation()
+loaderAnimation()
+
+
+
+
+
+const headings = document.querySelectorAll('.heading');
+const image = document.getElementById('images');
+const description = document.getElementById('description');
+const leftContainer = document.getElementById('left');
+
+function handleHeadingClick() {
+
+  headings.forEach(h => h.style.color='#bdb2b290');
+
+
+  description.classList.remove('selected-para');
+
+  const imageUrl = this.getAttribute('data-image');
+  
+ 
+  image.src = imageUrl;
+ 
+  image.alt = `Image ${imageUrl}`;
+
+
+  this.style.color='#fff';
+
+  description.classList.add('selected-para');
+
+
+  switch(this.textContent) {
+    case 'Design':
+      description.textContent = 'Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.';
+      break;
+    case 'Project':
+      description.textContent = 'Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.'; 
+      break;
+    case 'Execution':
+      description.textContent = 'We’re with you every step of the way, from the project initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.';
+      break;
+    default:
+      description.textContent = 'Our team works with our clients to refine an idea and concept into an executable design. We create a final design that encompasses the brand narrative to bring stories to life and provide end-to-end design solutions from concept, design, and architectural drawings to 3D renderings.';
+      break;
+  }
+
+}
+
+// Add click event listeners to each heading
+headings.forEach(heading => {
+  heading.addEventListener('click', handleHeadingClick);
+});
